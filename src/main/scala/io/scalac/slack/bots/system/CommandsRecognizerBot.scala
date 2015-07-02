3d3be1c3219c26c336ef51c473297ae036ebce08
Mariusz Nosiński <marioosh@5dots.pl>
2015-07-02 14:26:50 +0200

    Moved CommandsRecognizerBot into core

---
+++ src/main/scala/io/scalac/slack/bots/system/CommandsRecognizerBot.scala
@@ -0,0 +1,43 @@
+package io.scalac.slack.bots.system
+
+import io.scalac.slack.MessageEventBus
+import io.scalac.slack.bots.IncomingMessageListener
+import io.scalac.slack.common.{BaseMessage, BotInfoKeeper, Command}
+
+class CommandsRecognizerBot(override val bus: MessageEventBus) extends IncomingMessageListener {
+
+  val commandChar = '$'
+
+  def receive: Receive = {
+
+    case bm@BaseMessage(text, channel, user, dateTime, edited) =>
+      //COMMAND links list with bot's nam jack can be called:
+      // jack link list
+      // jack: link list
+      // @jack link list
+      // @jack: link list
+      // $link list
+      def changeIntoCommand(pattern: String): Boolean = {
+        if (text.trim.startsWith(pattern)) {
+          val tokenized = text.trim.drop(pattern.length).trim.split("\\s")
+          publish(Command(tokenized.head, tokenized.tail.toList.filter(_.nonEmpty), bm))
+          true
+        }
+        false
+      }
+
+      //call by commad character
+      if (!changeIntoCommand(commandChar.toString))
+        BotInfoKeeper.current match {
+          case Some(bi) =>
+            //call by name
+            changeIntoCommand(bi.name + ":") ||
+              changeIntoCommand(bi.name) ||
+              //call by ID
+              changeIntoCommand(s"<@${bi.id}>:") ||
+              changeIntoCommand(s"<@${bi.id}>")
+
+          case None => //nothing to do!
+        }
+  }
+}
